4210d4d33b9dae6f08dd24b24013e242ed8e26c7
Prashant Tomar <prashanttomar765@gmail.com>
2025-01-26 17:17:00 +0530

    Task completed button

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './task.model';
 @Component({
   selector: 'app-task',
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrl: './task.component.scss',
 })
 export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+  @Output() taskCompleted = new EventEmitter<string>();
 
+  taskCompletedClicked() {
+    this.taskCompleted.emit(this.task.id);
+  }
 }
