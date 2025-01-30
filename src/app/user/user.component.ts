d6c7de4548f95c4d74697f7f5072bbf05faa83f1
Prashant Tomar <prashanttomar765@gmail.com>
2025-01-30 20:58:10 +0530

    Card component to add style to multiple components

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-  @Input() isSelected! : boolean;
+  @Input() isSelected!: boolean;
 
   @Output() select = new EventEmitter<string>();
 
